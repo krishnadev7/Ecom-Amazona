d1a1966256b585b9346074f85328cb9c7b3b39c6
krishnadev v <krishnadevv12@gmail.comgit config --global user.name krishnadev7>
2022-08-21 12:03:22 +0530

    installed bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,32 @@
-import data from './data';
 import {BrowserRouter, Link, Route, Routes} from 'react-router-dom'
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import {LinkContainer} from 'react-router-bootstrap'
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
-        <header>
-          <Link to='/'>Amazona</Link>
-        </header>
+      <div className='d-flex flex-column site-container'>
+        <Navbar bg='dark' variant='dark'>
+          <Container>
+            <LinkContainer to='/'>
+              <Navbar.Brand>Amazona</Navbar.Brand>
+            </LinkContainer>
+          </Container>
+        </Navbar>
         <main>
-          <Routes>
-            <Route path='/product/:slug' element={<ProductScreen/>} />
-            <Route path='/' element={<HomeScreen/>}/>
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path='/product/:slug' element={<ProductScreen />} />
+              <Route path='/' element={<HomeScreen />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className='text-center'>All rights reserved</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
