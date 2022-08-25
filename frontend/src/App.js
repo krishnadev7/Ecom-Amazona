d3c81e784e33ba662cd890bfaf7b11e3f5939384
krishnadev v <krishnadevv12@gmail.comgit config --global user.name krishnadev7>
2022-08-25 19:58:25 +0530

    created Sign-In screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import Nav from 'react-bootstrap/Nav'
 import { useContext } from 'react';
 import { Store } from './screens/Store';
 import CartScreen from './screens/CartScreen';
+import SignInScreen from './screens/SignInScreen';
 
 function App() {
    const {state} = useContext(Store);
@@ -38,6 +39,7 @@ function App() {
             <Routes>
               <Route path='/product/:slug' element={<ProductScreen />} />
               <Route path='/cart' element={<CartScreen />} />
+              <Route path='/signin' element={<SignInScreen />} />
               <Route path='/' element={<HomeScreen />} />
             </Routes>
           </Container>
